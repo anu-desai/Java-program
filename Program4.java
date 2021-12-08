18d09903f3bb949c64060c1a2c67fee5c4ec860d
anudesai <anudesai98@gmail.com>
2021-12-07 21:41:55 -0800

    first commit

---
+++ Program4.java
@@ -0,0 +1,28 @@
+class Program4
+{
+	public static void main(String[] args) {
+		System.out.println("Program4 starts.....");
+		int n1=2;
+		int n2=7;
+		boolean res;
+		
+		res=n1>n2;
+		System.out.println("res:"+res);
+		
+		res=n1<n2;
+		System.out.println("res:"+res);
+		
+		res=n1==n2;
+		System.out.println("res:"+res);
+		
+		res=n1!=n2;
+		System.out.println("res:"+res);
+		
+		res=n1>=n2;
+		System.out.println("res:"+res);
+		
+		res=n1<=n2;
+		System.out.println("res:"+res);
+		System.out.println("Program4 ends.......");
+	}
+}
